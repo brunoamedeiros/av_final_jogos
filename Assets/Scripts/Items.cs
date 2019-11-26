c9906b62345cd26ba15ae281a1a49f1c4b0ca9fb
Bruno <bruno@dump.net.br>
2019-11-26 20:39:58 -0300

    alteração velocidade balao e passaro no nivel 2

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Items : MonoBehaviour
 {
   private GameController gameController;
-  private float speed = 3f;
+  public float speed = 3f;
   private float originalY;
   public string type;
   public AudioClip sound;
