38d8a4451ed0e6c25fec9799b42de47eac4353b6
Lucas dos Santos Madeira <lucas@MacBook-Air.local>
2019-11-27 23:46:32 -0300

    Ajustado posicao do balao e o label do start

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,188 +5,189 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-  public bool gameStarted;
-  public static int score;
-  private static int highestScore = 0;
-  public int currentHealth = 0;
-  public int maxHealth = 100;
-  private TextMesh ScoreText;
-  public int NewLevel = 1;
-  public float delay = 10;
-  public Scene currentScene;
-  public bool immunityActivated = false;
-  public SimpleHealthBar healthBar;
-  private float timer;
-  public AudioClip gameOverSound;
-  public AudioSource[] source;
-  public GameObject gameOverPanel;
-  public bool gameOver = false;
+    public bool gameStarted;
+    public static int score;
+    private static int highestScore = 0;
+    public int currentHealth = 0;
+    public int maxHealth = 100;
+    private TextMesh ScoreText;
+    public int NewLevel = 1;
+    public float delay = 10;
+    public Scene currentScene;
+    public bool immunityActivated = false;
+    public SimpleHealthBar healthBar;
+    private float timer;
+    public AudioClip gameOverSound;
+    public AudioSource[] source;
+    public GameObject gameOverPanel;
+    public bool gameOver = false;
 
-  private void Awake()
-  {
-    GameObject go = GameObject.FindWithTag("ScoreText");
-    ScoreText = go.GetComponent<TextMesh>();
-    source = GetComponents<AudioSource>();
-    currentScene = SceneManager.GetActiveScene();
-    gameOverPanel.SetActive(false);
-    ScoreText.gameObject.SetActive(false);
-    Screen.fullScreen = !Screen.fullScreen;
-    Screen.SetResolution(414, 736, false);
-  }
-
-  // Start is called before the first frame update
-  void Start()
-  {
-    if (currentScene.name == "Level 1")
+    private void Awake()
     {
-      gameStarted = false;
+        GameObject go = GameObject.FindWithTag("ScoreText");
+        ScoreText = go.GetComponent<TextMesh>();
+        source = GetComponents<AudioSource>();
+        currentScene = SceneManager.GetActiveScene();
+        gameOverPanel.SetActive(false);
+        ScoreText.gameObject.SetActive(false);
+        Screen.fullScreen = !Screen.fullScreen;
+        Screen.SetResolution(414, 736, false);
     }
 
-    currentHealth = maxHealth;
-
-    LoadPlayerProgress();
-    UpdateScore();
-    UpdateHealth();
-  }
-
-  void Update()
-  {
-    if (gameStarted && !gameOver)
+    // Start is called before the first frame update
+    void Start()
     {
-      AddScoreByTime();
-      ScoreText.gameObject.SetActive(true);
-    }
-  }
+        if (currentScene.name == "Level 1")
+        {
+            gameStarted = false;
+        }
 
-  public void AddScore(int newScoreValue)
-  {
-    score += newScoreValue;
-    UpdateScore();
-  }
+        currentHealth = maxHealth;
 
-  public void AddScoreByTime()
-  {
-    timer += Time.deltaTime;
-
-    if (timer > 3f)
-    {
-
-      score += 1;
-
-      UpdateScore();
-
-      //Reset the timer to 0.
-      timer = 0;
+        LoadPlayerProgress();
+        UpdateScore();
+        UpdateHealth();
     }
 
-    if (score >= 15 && currentScene.name == "Level 1")
+    void Update()
     {
-      SceneManager.LoadScene(1);
-    }
-  }
-
-  public void UpdateScore()
-  {
-    string text = "";
-
-    if (score < GetHighestPlayerScore())
-    {
-      text = score.ToString() + " / " + GetHighestPlayerScore().ToString();
-    }
-    else
-    {
-      text = score.ToString() + " / " + score.ToString();
+        if (gameStarted && !gameOver)
+        {
+            AddScoreByTime();
+            ScoreText.gameObject.SetActive(true);
+        }
     }
 
-    ScoreText.text = text;
-  }
-
-  // https://learn.unity.com/tutorial/live-session-quiz-game-2#5c7f8528edbc2a002053b634
-  public void SubmitNewPlayerScore(int newScore)
-  {
-    // If newScore is greater than playerProgress.highestScore, update playerProgress with the new value and call SavePlayerProgress()
-    if (newScore > highestScore)
+    public void AddScore(int newScoreValue)
     {
-      highestScore = newScore;
-      SavePlayerProgress();
+        score += newScoreValue;
+        UpdateScore();
     }
-  }
 
-  public int GetHighestPlayerScore()
-  {
-    return highestScore;
-  }
-
-  private void LoadPlayerProgress()
-  {
-    // If PlayerPrefs contains a key called "highestScore", set the value of highestScore using the value associated with that key
-    if (PlayerPrefs.HasKey("highestScore"))
+    public void AddScoreByTime()
     {
-      highestScore = PlayerPrefs.GetInt("highestScore");
+        timer += Time.deltaTime;
+
+        if (timer > 3f)
+        {
+
+            score += 1;
+
+            UpdateScore();
+
+            //Reset the timer to 0.
+            timer = 0;
+        }
+
+        if (score >= 15 && currentScene.name == "Level 1")
+        {
+            SceneManager.LoadScene(1);
+        }
     }
-  }
 
-  private void SavePlayerProgress()
-  {
-    // Save the value highestScore to PlayerPrefs, with a key of "highestScore"
-    PlayerPrefs.SetInt("highestScore", highestScore);
-  }
-  // https://learn.unity.com/tutorial/live-session-quiz-game-2#5c7f8528edbc2a002053b634
-
-  public void AddHealth()
-  {
-    // Increase the current health by 25%.
-    currentHealth += (maxHealth / 4);
-
-    // If the current health is greater than max, then set it to max.
-    if (currentHealth > maxHealth)
-      currentHealth = maxHealth;
-
-    // Update the Simple Health Bar with the new Health values.
-    UpdateHealth();
-  }
-
-  public void TakeDamage(int damage)
-  {
-    currentHealth -= damage;
-    UpdateHealth();
-  }
-
-  public void UpdateHealth()
-  {
-    // Now is where you will want to update the Simple Health Bar. Only AFTER the value has been modified.
-    healthBar.UpdateBar(currentHealth, maxHealth);
-
-    if (currentHealth < 0)
+    public void UpdateScore()
     {
-      GameOver();
+        string text = "";
+
+        if (score < GetHighestPlayerScore())
+        {
+            text = score.ToString() + " / " + GetHighestPlayerScore().ToString();
+        }
+        else
+        {
+            text = score.ToString() + " / " + score.ToString();
+        }
+
+        ScoreText.text = text;
     }
-  }
 
-  public void GameOver()
-  {
-    SubmitNewPlayerScore(score);
-    source[1].mute = true;
-    source[0].PlayOneShot(gameOverSound);
-    gameOverPanel.SetActive(true);
-    gameOver = true;
-  }
+    // https://learn.unity.com/tutorial/live-session-quiz-game-2#5c7f8528edbc2a002053b634
+    public void SubmitNewPlayerScore(int newScore)
+    {
+        // If newScore is greater than playerProgress.highestScore, update playerProgress with the new value and call SavePlayerProgress()
+        if (newScore > highestScore)
+        {
+            highestScore = newScore;
+            SavePlayerProgress();
+        }
+    }
 
-  public void ActiveImmunity()
-  {
-    StartCoroutine(Immunity());
-  }
+    public int GetHighestPlayerScore()
+    {
+        return highestScore;
+    }
 
-  IEnumerator Immunity()
-  {
-    immunityActivated = true;
-    healthBar.UpdateColor(new Color32(0, 94, 144, 255));
+    private void LoadPlayerProgress()
+    {
+        // If PlayerPrefs contains a key called "highestScore", set the value of highestScore using the value associated with that key
+        if (PlayerPrefs.HasKey("highestScore"))
+        {
+            highestScore = PlayerPrefs.GetInt("highestScore");
+        }
+    }
 
-    yield return new WaitForSeconds(10);
+    private void SavePlayerProgress()
+    {
+        // Save the value highestScore to PlayerPrefs, with a key of "highestScore"
+        PlayerPrefs.SetInt("highestScore", highestScore);
+    }
+    // https://learn.unity.com/tutorial/live-session-quiz-game-2#5c7f8528edbc2a002053b634
 
-    healthBar.UpdateColor(new Color32(47, 144, 0, 255));
-    immunityActivated = false;
+    public void AddHealth()
+    {
+        // Increase the current health by 25%.
+        currentHealth += (maxHealth / 4);
 
-    StopCoroutine("ActiveImmunity");
-  }
+        // If the current health is greater than max, then set it to max.
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        // Update the Simple Health Bar with the new Health values.
+        UpdateHealth();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateHealth();
+    }
+
+    public void UpdateHealth()
+    {
+        // Now is where you will want to update the Simple Health Bar. Only AFTER the value has been modified.
+        healthBar.UpdateBar(currentHealth, maxHealth);
+
+
+        if (currentHealth < 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        SubmitNewPlayerScore(score);
+        source[1].mute = true;
+        source[0].PlayOneShot(gameOverSound);
+        gameOverPanel.SetActive(true);
+        gameOver = true;
+    }
+
+    public void ActiveImmunity()
+    {
+        StartCoroutine(Immunity());
+    }
+
+    IEnumerator Immunity()
+    {
+        immunityActivated = true;
+        healthBar.UpdateColor(new Color32(0, 94, 144, 255));
+
+        yield return new WaitForSeconds(10);
+
+        healthBar.UpdateColor(new Color32(47, 144, 0, 255));
+        immunityActivated = false;
+
+        StopCoroutine("ActiveImmunity");
+    }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,95 +4,95 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-  public static float SpawnSpeed = 1.0f;
-  private float minY = -2.35f;
-  private float maxY = 3.76f;
-  public GameObject collectable;
-  public GameObject health;
-  public GameObject immunity;
-  public GameObject bird;
-  private GameController gameController;
+    public static float SpawnSpeed = 1.0f;
+    private float minY = -2.35f;
+    private float maxY = 3.76f;
+    public GameObject collectable;
+    public GameObject health;
+    public GameObject immunity;
+    public GameObject bird;
+    private GameController gameController;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    GameObject gameControllerObject = GameObject.FindWithTag("GameController");
-
-    if (gameControllerObject != null)
+    // Start is called before the first frame update
+    void Start()
     {
-      gameController = gameControllerObject.GetComponent<GameController>();
-    }
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
 
-    if (gameController == null)
-    {
-      Debug.Log("Cannot find 'GameController' script");
-    }
-
-    InvokeRepeating("SpawnItems", 0, SpawnSpeed);
-    InvokeRepeating("subIncreaseSpawnSpeed", 15, 15);
-  }
-
-  void subIncreaseSpawnSpeed()
-  {
-
-    float SpeedIncrease = 0.3f;
-
-    CancelInvoke("SpawnItems");
-
-    if ((SpawnSpeed - SpeedIncrease) < 0.1)
-    {
-      SpawnSpeed = 0.1f;
-    }
-    else
-    {
-      SpawnSpeed = SpawnSpeed - SpeedIncrease;
-    }
-
-    InvokeRepeating("SpawnItems", 0, SpawnSpeed);
-  }
-
-  void SpawnItems()
-  {
-    print(SpawnSpeed);
-    if (gameController.gameStarted && !gameController.gameOver)
-    {
-      int randomChanceItemOrObstacle = Random.Range(0, 100);
-      int randomChanceItem = Random.Range(0, 100);
-      int randomChanceObstacle = Random.Range(0, 100);
-
-      GameObject choosedItem = null;
-      Vector2 position = new Vector2(transform.position.x, Random.Range(minY, maxY));
-      Quaternion rotation = Quaternion.identity;
-
-      if (randomChanceItemOrObstacle <= 20)
-      {
-        // Items
-        if (randomChanceItem < 10)
+        if (gameControllerObject != null)
         {
-          choosedItem = immunity;
-          Spawn(choosedItem, position, rotation);
+            gameController = gameControllerObject.GetComponent<GameController>();
         }
-        else if (randomChanceItem < 25)
-        {
-          choosedItem = health;
-          Spawn(choosedItem, position, rotation);
-        }
-        else if (randomChanceItem < 60)
-        {
-          choosedItem = collectable;
-          Spawn(choosedItem, position, rotation);
-        }
-      }
-      else if (randomChanceItemOrObstacle >= 80)
-      {
-        choosedItem = bird;
-        Spawn(choosedItem, position, rotation);
-      }
-    }
-  }
 
-  void Spawn(GameObject item, Vector2 position, Quaternion rotation)
-  {
-    Instantiate(item, position, rotation);
-  }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+
+        InvokeRepeating("SpawnItems", 0, SpawnSpeed);
+        InvokeRepeating("subIncreaseSpawnSpeed", 15, 15);
+
+    }
+
+    void subIncreaseSpawnSpeed()
+    {
+
+        float SpeedIncrease = 0.3f;
+
+        CancelInvoke("SpawnItems");
+
+        if ((SpawnSpeed - SpeedIncrease) < 0.1)
+        {
+            SpawnSpeed = 0.1f;
+        }
+        else
+        {
+            SpawnSpeed = SpawnSpeed - SpeedIncrease;
+        }
+
+        InvokeRepeating("SpawnItems", 0, SpawnSpeed);
+    }
+
+    void SpawnItems()
+    {
+        if (gameController.gameStarted && !gameController.gameOver)
+        {
+            int randomChanceItemOrObstacle = Random.Range(0, 100);
+            int randomChanceItem = Random.Range(0, 100);
+            int randomChanceObstacle = Random.Range(0, 100);
+
+            GameObject choosedItem = null;
+            Vector2 position = new Vector2(transform.position.x, Random.Range(minY, maxY));
+            Quaternion rotation = Quaternion.identity;
+
+            if (randomChanceItemOrObstacle <= 20)
+            {
+                // Items
+                if (randomChanceItem < 10)
+                {
+                    choosedItem = immunity;
+                    Spawn(choosedItem, position, rotation);
+                }
+                else if (randomChanceItem < 25)
+                {
+                    choosedItem = health;
+                    Spawn(choosedItem, position, rotation);
+                }
+                else if (randomChanceItem < 60)
+                {
+                    choosedItem = collectable;
+                    Spawn(choosedItem, position, rotation);
+                }
+            }
+            else if (randomChanceItemOrObstacle >= 80)
+            {
+                choosedItem = bird;
+                Spawn(choosedItem, position, rotation);
+            }
+        }
+    }
+
+    void Spawn(GameObject item, Vector2 position, Quaternion rotation)
+    {
+        Instantiate(item, position, rotation);
+    }
 }
